fd5c50c496098b17ff8fa091807b714f74d2191f
Durgi Lokesh <lokeshgoud6218@gmail.com>
2026-06-18 20:49:00 +0530

    Operators in js

---
+++ operaters.js
@@ -0,0 +1,17 @@
+var a="10";
+var b=20;
+var res=a*b;
+console.log(res);
+
+var c=15;
+var d=4;
+var res1=c%d;
+var res2=c/d;
+var res3=c**d;
+
+console.log(res1); //modules get reminder
+console.log(res2); //division get quentont
+console.log(res3); //get exponent
+
+
+
